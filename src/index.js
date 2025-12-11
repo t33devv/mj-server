b8683cc0e63d78dd9d36fb280dd733772091b3ab
Tommy Zhou <tommyzhou03@gmail.com>
2025-12-11 09:45:30 +0800

    fix frontend url

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const cookieParser = require('cookie-parser');
 
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true,
+    credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
